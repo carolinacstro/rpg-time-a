244532c70e5930af82fb111784a845705297fee0
Grasiela <grasieduarda775@gmail.com>
2021-07-28 13:13:41 -0300

    Codigo funcionando

--- fase3.rb
+++ fase3.rb
@@ -1,10 +1,13 @@
-
 require "./logica_forca.rb"
 class Fase3
   def enigma3
-    puts "\nBem vindo ao terceiro enigma vamos ver se esse vc ira descobrir"
-    enigma3 = <<~TXT 
+    txt_intro = <<~TXT
     \n
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    ~~~~~~~~~~~~~~~~~~~ FASE 3 ~~~~~~~~~~~~~~~~~~~
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    TXT
+    txt_enigma = <<~TXT 
     "Se encontrar um bixo-papao, basta me conjurar
     Sua lembrança mais feliz, sempre te ajudará"
     \n
@@ -15,8 +18,8 @@ class Fase3
     (c) obliviate
     \n
     TXT
-    puts enigma3
-    i = 0
+    puts txt_enigma
+    erro = 0
     while i < 3
       resp = gets.chomp.upcase
       if resp == "B" && i = 3
@@ -29,35 +32,4 @@ class Fase3
       end
     end
   end
-end
-=======
-require "./logica.rb"
-puts "\nBem vindo ao terceiro enigma vamos ver se esse vc ira descobrir"
-enigma3 = <<~TXT 
-\n
-"Se encontrar um bixo-papao, basta me conjurar 
-Sua lembrança mais feliz, sempre te ajudará"
-\n
-Quem eu sou?
-\n
-(A)Expelliarmus
-(B)Expecto patronum
-(C)Obliviate
-\n
-TXT
-puts enigma3
-def fase3
-  print "Qual a resposta?"
-  resp = gets.strip.upcase
-  return resp
-end
-
-fase3
-resposta = fase3
-i = 0
-if resposta == "B" and i < 3
-  jogo_da_forca
-else 
-  i = i+1
-  fase3
 end
